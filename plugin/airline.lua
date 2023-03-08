f25d239cec153a461ba041e7da74fd56c85377cd
panda2k <thelowkeypanda@gmail.com>
2023-03-07 17:25:51 -0800

    Edit airline status again

--- plugin/airline.lua
+++ plugin/airline.lua
@@ -1,4 +1,6 @@
 vim.g["airline_section_x"] = ""
 vim.g["airline_section_y"] = ""
 vim.g["airline_section_x"] = ""
+vim.g["airline_section_z"] = ""
 vim.g["airline#extensions#tabline#enabled"] = 1
+vim.g["airline#extensions#whitespace#enabled"] = 0
