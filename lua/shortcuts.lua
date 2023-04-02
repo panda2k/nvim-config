a5c2104036f2572c2a434c8698b539eac390ea30
panda2k <thelowkeypanda@gmail.com>
2023-04-02 13:33:05 -0700

    Add shortcuts for fzf

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -6,8 +6,11 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map("n", "<C-k>", ":Files<CR>", { silent = true })
-map("n", "<c-l>", ":Rg<CR>", { silent = true})
+map("n", "<C-k>", ":GFiles<CR>", { silent = true })
+map("n", "<C-v>", ":bnext<CR>", { silent = true })
+map("n", "<C-c>", ":bprev<CR>", { silent = true })
+map("n", "<C-x>", ":bd<CR>", { silent = true })
+map("n", "<C-l>", ":Rg<CR>", { silent = true})
 map("n", "<C-j>", ":Trouble document_diagnostics<CR>", { silent = true })
 map("n", "<M>", ":bnext<CR>")
 map("n", "<N>", ":bprevious<CR>")
